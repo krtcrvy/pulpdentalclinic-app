ac572fa439dddb01ecd26cca002b73d47aa9a5fc
Kurt Carvey Cadenas <kurtcarveycadenas2001@gmail.com>
2023-03-04 00:22:11 +0800

    text size adjustment home.blade.php file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,7 @@
             </div>
             <div class="col-lg-6 blue-text text-primary">
                 <h2 class="display-5">Hello! This is</h2>
-                <h3 class="mb-4 display-5">PULP Dental Clinic</h3>
+                <h3 class="mb-4 display-3">PULP Dental Clinic</h3>
                 <p class="lead">
                     Our Liberty dentist office was built for people of all ages and celebrates the joy of a happy, healthy
                     smile. We combine cutting-edge dentistry with a team who treats you like family, a relaxing environment,
@@ -34,15 +34,16 @@
     </section>
     <section class="bg-primary">
         <div class="container px-4 py-5">
-            <h2 class="display-5 pb-2 text-center text-light">How could we help you?</h2>
+            <h2 class="display-4 pb-2 text-center text-light">How could we help you?</h2>
             <div class="row row-cols-1 row-cols-lg-4 align-items-stretch g-4 py-5">
                 <div class="col">
                     <div class="card mb-4 rounded-3 shadow-sm h-100">
                         <div class="card-body d-flex justify-content-between align-items-center flex-column p-5">
-                            <h2 class="h3 text-primary">
+                            <h3 class="text-primary">
                                 Time for a checkup?
-                            </h2>
-                            <p>We've got you covered with personalized cleanings, painless fillings, crowns, dentures, and
+                            </h3>
+                            <p class="lead">We've got you covered with personalized cleanings, painless fillings, crowns,
+                                dentures, and
                                 bridges.</p>
                             <button type="button" class="w-100 btn btn-lg btn-primary">Learn more</button>
                         </div>
@@ -51,10 +52,11 @@
                 <div class="col">
                     <div class="card mb-4 rounded-3 shadow-sm h-100">
                         <div class="card-body d-flex justify-content-between align-items-center flex-column p-5">
-                            <h2 class="h3 text-primary">
+                            <h3 class="text-primary">
                                 Fix damaged teeth
-                            </h2>
-                            <p>Expert surgical care from the team you know and trust. Implants, canals, extractions and
+                            </h3>
+                            <p class="lead">Expert surgical care from the team you know and trust. Implants, canals,
+                                extractions and
                                 more.
                             </p>
                             <button type="button" class="w-100 btn btn-lg btn-primary">Learn more</button>
@@ -64,10 +66,11 @@
                 <div class="col">
                     <div class="card mb-4 rounded-3 shadow-sm h-100">
                         <div class="card-body d-flex justify-content-between align-items-center flex-column p-5">
-                            <h2 class="h3 text-primary">
+                            <h3 class="text-primary">
                                 Improve your smile
-                            </h2>
-                            <p>We'll help you understand high-tech cosmetic options, like Invisalign, veneers, teeth
+                            </h3>
+                            <p class="lead">We'll help you understand high-tech cosmetic options, like Invisalign,
+                                veneers, teeth
                                 whitening, and Botox.</p>
                             <button type="button" class="w-100 btn btn-lg btn-primary">Learn more</button>
                         </div>
@@ -76,10 +79,11 @@
                 <div class="col">
                     <div class="card mb-4 rounded-3 shadow-sm h-100">
                         <div class="card-body d-flex justify-content-between align-items-center flex-column p-5">
-                            <h2 class="h3 text-primary">
+                            <h3 class="text-primary">
                                 Get to know us
-                            </h2>
-                            <p>Led by Dr. Raul Arganza, our team is proud to offer a fresh take on going to the dentist in
+                            </h3>
+                            <p class="lead">Led by Dr. Raul Arganza, our team is proud to offer a fresh take on going to
+                                the dentist in
                                 Liberty.</p>
                             <button type="button" class="w-100 btn btn-lg btn-primary">Learn more</button>
                         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.index');
-});
+Auth::routes();
 
-Route::get('/admin', function () {
-    return view('admin.index');
-});
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
